789cce903160a8b30aed06fc5217203ad0b396bb
Yuan Luo <chengxuyuan1998@gmail.com>
2023-03-30 10:54:02 +0800

    add reduction1

---
+++ reduction.cu
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+__global__ void reduce1(int *g_idata, int *g_odata) {
+   extern __shared__ int sdata[];
+
+   unsigned int tid = threadIdx.x;
+   unsigned int i = threadIdx.x + blockIdx.x * blockDim.x;
+   sdata[tid] = g_idata[tid];
+   __syncthreads();
+
+   for (unsigned int s = 1; s < blockDim.x; s *= 2) {
+      if (tid % (2*s) == 0) {
+       sdata[tid] += sdata[tid + s];
+      }
+      __syncthreads();
+   }
+   
+   if (tid == 0) g_odata[blockIdx.x] = sdata[0];
+}
+
+
+int main() {
+
+}
